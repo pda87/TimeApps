97083af123018d1817c865c7154a180167b45ac2
Pete Arden <pete.davidarden@gmail.com>
2015-04-04 15:21:48 +0100

    Removed Console.CapsLock logic from Timer.

--- Timer/Timer/Program.cs
+++ Timer/Timer/Program.cs
@@ -15,7 +15,6 @@ namespace Timer
             Console.Title = "Timer";
             Console.SetWindowSize(49, 10);
 
-
             Console.WriteLine("TTTTTTT IIIII MM    MM EEEEEEE RRRRRR");
             Console.WriteLine("  TTT    III  MMM  MMM EE      RR   RR");
             Console.WriteLine("  TTT    III  MM MM MM EEEEE   RRRRRR");
@@ -24,13 +23,6 @@ namespace Timer
 
             Console.WriteLine();
             Console.WriteLine();
-
-            if (Console.CapsLock)
-            {
-                Console.WriteLine("Please restart the program and turn off caps lock...");
-                Console.ReadLine();
-                return;
-            }
 
             Console.WriteLine("Please enter the time to countdown from (HHMMSS):");
             string timerInput = Console.ReadLine();
@@ -57,10 +49,8 @@ namespace Timer
 
             DateTime zerothHour = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, HH, MM, SS);
 
-            string loopBreak = "";
-            int i = 0;
-
             Console.Clear();
+            
             while (true)
             {
                 if (zerothHour.Hour == 0 && zerothHour.Minute == 0 && zerothHour.Second == 0)
@@ -73,66 +63,12 @@ namespace Timer
                     break;
                 }
 
+                System.Threading.Thread.Sleep(1000);
 
-
-                if (i != 1)
-                {
-                    Console.SetCursorPosition(0, 3);
-                    Console.WriteLine("Press caps lock to stop the timer...");
-                }
-
-                if (Console.CapsLock)
-                {
-                    while (true)
-                    {
-                        Console.SetCursorPosition(0, 3);
-                        Console.WriteLine("Press caps lock then enter to start the timer...");
-                        Console.SetCursorPosition(0, 4);
-                        Console.WriteLine("Press caps lock, Q then enter to quit");
-                        loopBreak = Console.ReadLine();
-
-                        if (!Console.CapsLock)
-                        {
-                            Console.Clear();
-                            break;
-                        }
-                    }
-                }
-
-                if (i == 1)
-                {
-                    break;
-                }
-
-                if (loopBreak.ToUpper() == "Q")
-                {
-                    i++;
-                }
-
-                if (i != 1)
-                {
-                    System.Threading.Thread.Sleep(1000);
-                }
-                
                 Console.SetCursorPosition(0, 0);
-                Console.WriteLine(String.Format("{0:00}:{1:00}.{2:00}", zerothHour.Hour, zerothHour.Minute, zerothHour.Second));
+                Console.WriteLine(String.Format("Hours:   {0:00}\r\nMinutes: {1:00}\r\nSeconds: {2:00}", zerothHour.Hour, zerothHour.Minute, zerothHour.Second));
                 Console.WriteLine();
                 zerothHour = zerothHour.AddSeconds(-1.0);
-            }
-
-
-            Console.SetCursorPosition(0, 2);
-            Console.WriteLine();
-            Console.WriteLine("Press enter to quit, or type 'r' or 'restart' to restart");
-            Console.WriteLine("(make sure you turn off caps lock)");
-            string input = Console.ReadLine().ToUpper();
-            if (input == "R" || input == "RESTART")
-            {
-                Main(args);
-            }
-            else
-            {
-                return;
             }
         }
     }
